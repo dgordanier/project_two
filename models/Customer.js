45cc25bd31847c3a357e21e554836e2035a0efef
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-01-15 10:49:18 -0800

    finished customer information schema

---
+++ models/Customer.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema (
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true},
+        shippingAddress: {
+            street: {type: String, required: true},
+            city: { type: String, required: true},
+            state: {
+                type: String,
+                uppercase: true,
+                required: true,
+                enum: statesArray
+            },
+            zip: Number
+        },
+        billingAddress: {
+            street: {type: String, required: true},
+            city: { type: String, required: true},
+            state: {
+                type: String,
+                uppercase: true,
+                required: true,
+                enum: statesArray
+            },
+            zip: Number
+        },
+        creditCard: {type: Number, required: true},
+        expiration: {type: Date},
+        CVV: {type: Number},
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Customer = mongoose.model('Customer', customerSchema);
+module.exports = Customer;
