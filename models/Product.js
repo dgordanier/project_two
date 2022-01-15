210b4b514f53d02de0e43a51cc7874af7f83e77d
Denzel <denzelsamala@Denzel.local>
2022-01-14 20:35:54 -0800

    adjusted Product Schema and blank index template. informed david of commit to main branch

--- models/Product.js
+++ models/Product.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema (
     {
         name: {type: String, required: true},
-        price: {type: int, required: true},
+        price: {type: Number, required: true},
         description: {type: String, required: true},
-        rating: {type: int, required: true}, 
+        rating: {type: Number, required: true}, 
         image: {type: String, required: true},
     },
     {
